b64d5087682060a6132be357a6562811767418e6
savaariuttam <uttam@onlinesavaari.com>
2024-05-05 11:29:12 +0530

    some changes in details css

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-import DetailsHeader from '../details/DetailsHeader';
-import Footer from './Footer';
-import '../components/css/details.css'
-const DetailsPage=()=> {
+import DetailsHeader from "../details/DetailsHeader";
+import Header from "../components/header";
+import Footer from "./Footer";
+import "../components/css/details.css";
+const DetailsPage = () => {
   return (
     <>
-    <DetailsHeader />
-    <Footer />
+      <Header />
+      <DetailsHeader />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default DetailsPage;
-
-
-
