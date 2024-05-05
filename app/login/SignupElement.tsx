240b79e9d562a5d0c481b0812646362dd4d7b581
savaariuttam <uttam@onlinesavaari.com>
2024-05-05 08:43:47 +0530

    some changes in signupelement

--- app/login/SignupElement.tsx
+++ app/login/SignupElement.tsx
@@ -9,12 +9,12 @@ const SignupElement = () => {
                 <h3>Create an account!</h3>
                 <p>Fill the form to create an account</p>
             </div>
-                <Input label="First Name" type="text" name="Firstname" placeholder="First Name" required />
-                <Input label="Last Name" type="text" name="Lastname" placeholder="Last Name" required />
-                <Input label="Email ID" type="email" name="Email" placeholder="Email ID" required />
-                <Input label="Phone Number" type="tel" name="phone" placeholder="Phone Number" maxLength="10" />
-                <Input label="Password" type="password" name="Password" placeholder="Password******" required />
-                <Input label="Confirm Password" type="password" name="Password1" placeholder="******" required />
+                <Input label="First Name" type="text" name="Firstname" placeholder="First Name"maxLength required />
+                <Input label="Last Name" type="text" name="Lastname" placeholder="Last Name" maxLength required />
+                <Input label="Email ID" type="email" name="Email" placeholder="Email ID" maxLength required />
+                <Input label="Phone Number" type="tel" name="phone" placeholder="Phone Number" maxLength="10" required/>
+                <Input label="Password" type="password" name="Password" placeholder="Password******"maxLength required />
+                <Input label="Confirm Password" type="password" name="Password1" placeholder="******" maxLength required />
                 <div className="form-group">
                     <input className="btn btn-block btn-blue form-control" type="submit" value="Create An Account " />
                 </div>
