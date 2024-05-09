fc268c6bf401eeb3d3aa779ebe04db2c09e65ab3
savaariuttam <uttam@onlinesavaari.com>
2024-05-09 12:33:15 +0530

    some changes im css

--- app/cancel/routeDetails.tsx
+++ app/cancel/routeDetails.tsx
@@ -6,7 +6,7 @@ const RouteDetails = () => {
   return (
     <div className="block w-full overflow-auto scrolling-touch">
       <form action="/flight/cancel_booking/" method="POST">
-        <table className="w-full max-w-full mb-4 bg-transparent table-bordered table-hover table-striped table-checkable table-highlight-head mb-4">
+        <table className="w-full max-w-full bg-transparent table-bordered table-hover table-striped table-checkable table-highlight-head mb-4">
           <tbody>
             <tr>
               <th className="checkbox-column">
