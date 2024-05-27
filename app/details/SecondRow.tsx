66208814e37daa1e3c5a4634750ff0b88b8630e3
savaariuttam <uttam@onlinesavaari.com>
2024-05-27 19:09:41 +0530

    datepicker add in details page

--- app/details/SecondRow.tsx
+++ app/details/SecondRow.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 const SecondRow = () => {
+    const currentDate = new Date();
+    const [departureDate, setDepartureDate] = useState(currentDate);
+
+
+    const handleDepartureDateChange = (date) => {
+        setDepartureDate(date);
+    };
+
+ 
     return (
         <div className="sm:w-full md:w-full lg:w-full">
             <div className="flight-input1">
                 <div className="flight-group1">
-                    <label htmlFor="travelDate">Departure Date</label>
-                    <input type="text" className="datepicker" />
+                    <label className="text-xs leading-4 mb-1 text-white inline-block max-w-full font-normal" htmlFor="startDate">Departure Date</label>
+                    <DatePicker
+                        selected={departureDate}
+                        onChange={handleDepartureDateChange}
+                        dateFormat="dd-MM-yyyy"
+                        className="datepicker"
+                        id="startDate"
+                        name="departureDate"
+                        minDate={currentDate}
+                    />
                 </div>
             </div>
         </div>
+
+
+
+
     );
 };
 
